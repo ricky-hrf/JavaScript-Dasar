d06dbdd1278a14e73cdb4c0c0096060abfa1f21d
ricky-hrf <riskiharefa2000@gmail.com>
2024-09-05 12:04:59 +0700

    Belajar Array

---
+++ 14 js arrays/script.js
@@ -0,0 +1,22 @@
+//membuat array 
+const mobil = ['ertiga', 'avanza', 'calya'];
+document.getElementById("demo").innerHTML = mobil;
+const mobilku = new Array ('HRV', 'stargazer', 'innova');
+document.getElementById("output").innerHTML = "Saya akan Memiliki Mobil ini : " + mobilku;
+
+//mengakses elemen
+let mbl = mobilku[0];
+document.getElementById("mbl").innerHTML = "Index pertama dari array tersebut adalah: " + mbl;
+
+//mengubah nilai array
+mobilku[2] = 'BMW';
+document.getElementById("change").innerHTML = "Element array index ke-2 diubah menjadi : " + mobilku;
+
+//konversi array ke string
+const buah = ["apel", "pear", "pepaya"];
+document.getElementById("buahkita").innerHTML = "array buah = " + buah.toString();
+
+// properti dan method array
+    //length
+    let size = buah.length;
+    document.getElementById("jumlahelemen").textContent = "jumlah elemen dari array buah adalah : " + size;
