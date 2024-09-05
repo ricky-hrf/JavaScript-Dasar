318437636696ada98e257100b7eb436ca24bc2e4
ricky-hrf <riskiharefa2000@gmail.com>
2024-09-06 00:10:57 +0700

    lanjut mempelajari array methods

--- 14 js arrays/script.js
+++ 14 js arrays/script.js
@@ -17,6 +17,37 @@ const buah = ["apel", "pear", "pepaya"];
 document.getElementById("buahkita").innerHTML = "array buah = " + buah.toString();
 
 // properti dan method array
-    //length
+    //length untuk menghitung jumlah elemen dalam array
     let size = buah.length;
     document.getElementById("jumlahelemen").textContent = "jumlah elemen dari array buah adalah : " + size;
+
+//looping element array
+    //contoh pertama
+    const mahasiswa = ["Budi", "Ayu", "Lesta", "Jerry"];
+    let mhslen = mahasiswa.length;
+    let text = "<ul>";
+        for (let i = 0; i < mhslen; i++){
+            text += "<li>" + mahasiswa[i] + "</li>";
+        }
+        text += "</ul>";
+    document.getElementById("looping").innerHTML = "Hasil dari penggunaan looping :" + "<br>" + text;
+    //contoh kedua menggunakan function Array.forEach()
+    let text2 = "<ul>";
+        mahasiswa.forEach(myFunction);
+    text2 += "</ul>";
+    function myFunction(value){
+        text2 += "<li>" + value + "</li>";
+    };
+    document.getElementById("looping2").innerHTML = "hasil looping menggunakan forEach : "+"<br>"+text2;
+
+//menambahkan elemen array
+    //menggunakan push() method
+    function tambahArray(){
+        mahasiswa.push("Brian");
+        document.getElementById("tambah").innerHTML = "array mahasiswa setelah di tambah :" + mahasiswa;
+    };
+    //menggunakan length
+    function tbarlength(){
+        mahasiswa[mahasiswa.length] = "George";
+        document.getElementById("tambah2").innerHTML = "Tambah elemen array menggunakan length :" + mahasiswa;
+    };
---
+++ 15 js array methods/script.js
@@ -0,0 +1,62 @@
+//array methods dasar
+  //array length, mengembalikan size atau ukuran array
+  const buah = ["pisang", "Jeruk", "Apel", "Mangga"];
+  let size = buah.length;
+  document.getElementById("buah").innerHTML = "hasil array methods menggunakan length : " + size;
+
+  //array toString(), mengubah array menjadi string
+  document.getElementById("toString").innerHTML = "Hasil array methods menggunakan toString() : " + buah.toString();
+
+  //array at(), mengembalikan index elemen array
+  let buahku = buah.at(2);
+  document.getElementById("buahku").innerHTML = "Hasil array methods menggunakan at() : " + buahku;
+
+  // array join(), menambah karakter pemisah elemen array
+  document.getElementById("join").innerHTML = "Hasil array methods menggunakan join() : " + buah.join("-");
+
+  //array pop(), menghapus elemen terakhir pada array
+  buah.pop();
+  document.getElementById("array").innerHTML = "Hasil array methods menggunakan pop() : " + buah;
+
+  //array push(), menambah elemen pada array
+  buah.push("Kiwi");
+  document.getElementById("push").innerHTML = "Hasil array methods menggunakan pop() : " + buah;
+
+  //array shift(), menghapus elemen pertama array
+  buah.shift();
+  document.getElementById("shift").innerHTML = "Hasil array methods menggunakan shift() : " + buah;
+
+  //array unshift(), menambahkan elemen pada bagian awal array
+  buah.unshift("Lemon");
+  document.getElementById("unshift").innerHTML = "Hasil array methods menggunakan unshift() : " + buah;
+
+  //array concat()/Concatenating, membuat array baru dari gabungan array
+      //contoh 1
+      const arr1 = ["Cecilion", "Lone"];
+      const arr2 = ["Emil", "Tobias", "Linus"];
+      const arr3 = ["Robin", "Morgan"];
+      const myChildren = arr1.concat(arr2, arr3);
+      document.getElementById("concat").innerHTML = "Hasil array methods contoh 1 menggunakan concat() : " + myChildren;
+      //contoh 2, menggabungkan nilai string ke dalam array
+      const MyChildren = arr1.concat("Peter");
+      document.getElementById("concat2").innerHTML = "Hasil array methods contoh 2 menggunakan concat() : " + MyChildren;
+
+  //array copyWithin(), menduplikat elemen array ke posisi yang lain di dalam array
+  document.getElementById("copy").innerHTML = "Hasil array methods menggunakan copyWithin() : " + buah.copyWithin(2,0);
+
+  //array flat(), membuat array baru dengan elemen sub-array
+  const myArr = [[1,2], [3,4], [5,6]];
+  const newArr = myArr.flat();
+  document.getElementById("flat").innerHTML = "Hasil array methods menggunakan flat() : " + newArr;
+
+  //array flatMap()
+  const bilangan = [1,2,3,4,5,6];
+  const newBilangan = bilangan.flatMap(x => [x, x * 10]);
+  document.getElementById("flatmap").innerHTML = "Hasil array methods menggunakan flatMap() : " + newBilangan;
+
+  //array splice(), bisa digunakan untuk menambah item baru di dalam array
+  buah.splice(2,0, "Lemon", "Kiwi");
+  document.getElementById("splice").innerHTML = "Hasil array methods menggunakan splice() : " + buah;
+
+  //array tosplice()
+  
