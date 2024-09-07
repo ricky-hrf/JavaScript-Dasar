e019e1d8cd0fed497ad985596d305d0a879d5ca4
ricky-hrf <riskiharefa2000@gmail.com>
2024-09-07 13:33:06 +0700

    Belajar Penganggalan

--- 19 js dates/script.js
+++ 19 js dates/script.js
@@ -1,1 +1,10 @@
 //new date()
+const d = new Date();
+document.getElementById("demo").innerHTML = "Hasil JavaScript menggunakan Date() : " + d;
+
+//new Date(string), membuat objek dari date berbentuk string
+const dt = new Date("2017-12-25");
+document.getElementById("demo1").innerHTML = dt;
+
+const dat = new Date(2018, 11, 24, 10, 33, 30, 0);
+document.getElementById("demo2").innerHTML = dat;
